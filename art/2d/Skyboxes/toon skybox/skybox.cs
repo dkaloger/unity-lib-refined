e4f33e57d578c586f6f940bb8d9e91173204e958
dkaloger <52400814+dkaloger@users.noreply.github.com>
2020-07-24 22:47:41 +0300

    removed placeholders and  added the toon skybox

---
+++ art/2d/Skyboxes/toon skybox/skybox.cs
@@ -0,0 +1,71 @@
+﻿//includes rotating and daynight cycle
+using UnityEngine;
+public class skybox :MonoBehaviour
+{
+
+    [SerializeField]
+    bool rotate;
+    public float RotationPerSecond = 1;
+
+    [SerializeField] 
+    bool day_night_cycle;
+    [SerializeField]
+    int day_length = 300;
+     int time;
+    int t;
+    bool ascending=true;
+
+    [SerializeField]
+    Material[] Skyboxes;
+
+
+
+ void Start()
+    {
+        ascending = true;
+        time = PlayerPrefs.GetInt("time");
+    }
+
+    void FixedUpdate()
+    {
+
+        if (day_night_cycle) {
+       
+            t++;
+
+            if (t > 60 - 1)
+            {
+                PlayerPrefs.SetInt("time", time);
+                if (ascending)
+                {
+                    time++;
+                }
+                if (!ascending)
+                {
+                    time--;
+                }
+
+                RenderSettings.skybox = Skyboxes[Mathf.RoundToInt(time / (day_length / Skyboxes.Length + 1))];
+                t = 0;
+            }
+
+            if (time > day_length - 1)
+            {
+                ascending = false;
+            }
+            if (time == 0)
+            {
+                ascending = true;
+            }
+        }
+
+
+        if (rotate)
+        {
+            RenderSettings.skybox.SetFloat("_Rotation", Time.time * RotationPerSecond);
+
+        }
+
+    }
+
+}
